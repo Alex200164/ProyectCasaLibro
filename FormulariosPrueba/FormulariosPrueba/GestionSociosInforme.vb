d0be652e3140cd29ddd22b4466ae5c24b0efb908
AlexM <alexm200164@gmail.com>
2021-02-25 15:43:19 +0100

    El maldito reportViewer
    Consegui qeu funcionase

---
+++ FormulariosPrueba/FormulariosPrueba/GestionSociosInforme.vb
@@ -0,0 +1,40 @@
+﻿' Necesitamos importar el módelo de base de datos que vamos a utilizar, este es de access.
+Imports System.Data.OleDb
+
+Public Class GestionSociosInforme
+
+    ' Especificamos la base de datos a la que nos vamos a conectar.
+    Public conexion As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=cas_lib_dib.accdb")
+    ' Al adaptador le asignamos la conexion que acabamos de realizar y una consulta
+    Public adaptador As New OleDbDataAdapter("Select * from Socios", conexion)
+
+    Private Sub GestionSociosInforme_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Using con As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=cas_lib_dib.accdb")
+            con.Open()
+            Using cmd As New OleDbCommand("Select * from Socios", con)
+                Dim da As New OleDbDataAdapter
+
+                da.SelectCommand = cmd
+
+                Dim dt As New DataSet
+
+                da.Fill(dt)
+
+            End Using
+        End Using
+
+        With Me.ReportViewer1.LocalReport
+            .DataSources.Clear()
+            .ReportPath = "GestionSociosInforme.rdlc"
+        End With
+
+
+
+        Me.ReportViewer1.RefreshReport()
+    End Sub
+
+    Private Sub ReportViewer1_Load(sender As Object, e As EventArgs) Handles ReportViewer1.Load
+
+    End Sub
+End Class
--- FormulariosPrueba/FormulariosPrueba/My Project/Application.Designer.vb
+++ FormulariosPrueba/FormulariosPrueba/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.FormulariosPrueba.GestionLibros
+            Me.MainForm = Global.FormulariosPrueba.InicioSesion
         End Sub
     End Class
 End Namespace
